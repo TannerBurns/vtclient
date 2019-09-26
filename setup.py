56d5dcbe13d7c0ab008a9ac1d5ca10587d7108d6
Tanner Burns <tburns@cylance.com>
2019-09-26 08:52:51 -0500

    fixed download returns and added http mount

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='VtClient',  
-     version='1.0',
+     version='1.1',
      author="Tanner Burns",
      author_email="tjburns102@gmail.com",
      description="An asynchronous client for VirusTotal",
