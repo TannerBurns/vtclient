969890d91f30c482569e0cf436a3719541187851
Tanner Burns <tburns@cylance.com>
2019-10-21 13:05:40 -0500

    made project structure more simple for vtclient package

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='VtClient',  
-     version='1.1',
+     version='1.1.2',
      author="Tanner Burns",
      author_email="tjburns102@gmail.com",
      description="An asynchronous client for VirusTotal",
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/tannerburns/vtclient",
-     package_dir={'':'src'},
-     packages=setuptools.find_packages("src"),
+     packages=setuptools.find_packages(),
      include_package_data=True,
      install_requires=[
          "requests"
