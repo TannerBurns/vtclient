7e1e5b5c7a8e28377b9e0c79c64e0534d012804d
Tanner Burns <tburns@cylance.com>
2019-10-24 11:07:23 -0500

    added aiorequests to setup.py, removed hard coded aiorequests

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
      packages=setuptools.find_packages(),
      include_package_data=True,
      install_requires=[
-         "requests"
+         "aiorequests @ https://github.com/TannerBurns/aiorequests/archive/master.zip"
      ],
      classifiers=[
          "Programming Language :: Python :: 3",
--- vtclient/vtclient.py
+++ vtclient/vtclient.py
@@ -8,97 +8,7 @@ from functools import partial
 from typing import Callable, Tuple, List
 from urllib.parse import urlparse, parse_qs
 
-import requests
-
-
-class AioRequests(object):
-    def __init__(self, workers: int= 16, *args: list, **kwargs: dict):
-        self.workers = workers
-        self.session = requests.Session()
-        rqAdapters = requests.adapters.HTTPAdapter(
-            pool_connections = workers, 
-            pool_maxsize = workers+4, 
-            max_retries = 3
-        )
-        self.session.mount("https://", rqAdapters)
-        self.session.mount('http://', rqAdapters)
-        self.session.headers.update({
-                "Accept-Encoding": "gzip, deflate",
-                "User-Agent" : "gzip,  Python Asyncio Requests Client"
-        })
-        self.basepath = os.path.realpath(os.getcwd())
-
-    async def _execute(self, fn: Callable, *args: list, **kwargs: dict):
-        return fn(*args, **kwargs)
-    
-    async def async_get(self, *args, **kwargs):
-        return await self._execute(self.session.get, *args, **kwargs)
-    
-    async def async_post(self, *args, **kwargs):
-        return await self._execute(self.session.post, *args, **kwargs)
-    
-    async def async_put(self, *args, **kwargs):
-        return await self._execute(self.session.put, *args, **kwargs)
-    
-    async def async_delete(self, *args, **kwargs):
-        return await self._execute(self.session.delete, *args, **kwargs)
-
-    async def async_head(self, *args, **kwargs):
-        return await self._execute(self.session.head, *args, **kwargs)
-
-    def get(self, *args, **kwargs):
-        return self.session.get(*args, **kwargs)
-    
-    def post(self, *args, **kwargs):
-        return self.session.post(*args, **kwargs)
-
-    def put(self, *args, **kwargs):
-        return self.session.put(*args, **kwargs)
- 
-    def delete(self, *args, **kwargs):
-        return self.session.delete(*args, **kwargs)
-    
-    def head(self, *args, **kwargs):
-        return self.session.head(*args, **kwargs)
-  
-    async def _aio_executor(self, method: str, calls: List[Tuple[list, dict]]):
-        if method.lower() == 'get':
-            return [
-                await self.async_get(*argtuple[0], **argtuple[1]) 
-                for index in range(0, len(calls), self.workers) 
-                for argtuple in calls[index:index+self.workers]
-            ]
-        elif method.lower() == 'post':
-            return [
-                await self.async_post(*argtuple[0], **argtuple[1]) 
-                for index in range(0, len(calls), self.workers)
-                for argtuple in calls[index:index+self.workers]
-            ]
-        elif method.lower() == 'put':
-            return [
-                await self.async_put(*argtuple[0], **argtuple[1]) 
-                for index in range(0, len(calls), self.workers) 
-                for argtuple in calls[index:index+self.workers]
-            ]
-        elif method.lower() == 'delete':
-            return [
-                await self.async_delete(*argtuple[0], **argtuple[1]) 
-                for index in range(0, len(calls), self.workers) 
-                for argtuple in calls[index:index+self.workers]
-            ]
-        elif method.lower() == 'head':
-            return [
-                await self.async_head(*argtuple[0], **argtuple[1]) 
-                for index in range(0, len(calls), self.workers) 
-                for argtuple in calls[index:index+self.workers]
-            ]
-        else:
-            raise Exception(f'Method "{method}" not supported. Choices: get, post, put, delete, head')
-
-    def bulk_request(self, method: str, calls: List[Tuple[list, dict]]):
-        loop = asyncio.new_event_loop()
-        return loop.run_until_complete(self._aio_executor(method, calls))
-
+from aiorequests import AioRequests
 
 class VTClient(AioRequests):
     def __init__(self, vtkey:str, download_directory:str="downloads", *args:list, **kwargs:dict):
@@ -120,12 +30,12 @@ class VTClient(AioRequests):
             for index in range(0, len(hashlist), resource_chunk)
         ]
         calls = [
-            ([url], {'params': {"apikey" : self.vtkey, "resource" : group, "allinfo" : allinfo}}) 
+            ('get', url, {'params': {"apikey" : self.vtkey, "resource" : group, "allinfo" : allinfo}}) 
             for group in resource_groups
         ]
         return {
             resp.get('sha256', resp.get('resource')):resp
-            for responses in self.bulk_request('get', calls)
+            for responses in self.bulk_requests(calls)
             for resp in responses.json()
         }
     
@@ -180,8 +90,8 @@ class VTClient(AioRequests):
         if not os.path.exists(self.download_directory):
             os.makedirs(self.download_directory)
         url = "https://www.virustotal.com/intelligence/download/"
-        calls = [([url], {'params': {'apikey': self.vtkey, 'hash': hashval}}) for hashval in hashlist]
-        bulk_responses = self.bulk_request('get', calls)
+        calls = [('get', url, {'params': {'apikey': self.vtkey, 'hash': hashval}}) for hashval in hashlist]
+        bulk_responses = self.bulk_requests(calls)
         results = []
         for response in bulk_responses:
             if response.status_code == 200:
